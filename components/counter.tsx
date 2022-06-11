5a1858dd2bef19c8791c72b9726b5ba408764603
again7536 <45936642+again7536@users.noreply.github.com>
2022-06-11 19:23:04 +0900

    initial commit

---
+++ components/counter.tsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return <div>hi</div>;
+}
---
+++ components/test.tsx
@@ -0,0 +1,8 @@
+import Counter from './counter';
+import styled from 'styled-components';
+
+const StyledCounter = styled(Counter)`
+  color: red;
+`;
+
+export default StyledCounter;
