0da9452bc8fb146ff347a9715be364af8825c1ad
scg <scg@scgui-Macmini.Dlink>
2022-06-12 14:08:02 +0900

    [Add] storybook

--- components/counter.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react';
-
-export default function Counter() {
-  const [count, setCount] = useState(0);
-  return <div>hi</div>;
-}
--- components/test.tsx
+++
@@ -1,8 +0,0 @@
-import Counter from './counter';
-import styled from 'styled-components';
-
-const StyledCounter = styled(Counter)`
-  color: red;
-`;
-
-export default StyledCounter;
