315739722eba733f3a78c6d69cc39c3edf39ffb2
again7536 <45936642+again7536@users.noreply.github.com>
2022-06-12 22:08:08 +0900

    [Fix] pages api

--- pages/api/pages.ts
+++ pages/api/pages.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
 
 type Data = {
-  name: string;
+  fileList: string[];
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  const fileList = fs.readdirSync('/posts');
+  res.status(200).json({ fileList: fileList });
 }
